855aaf483320144ab6770cc3ac8d60277ebab353
Benjamin Beret <benjamin.beret@gmail.com>
2015-04-30 00:58:23 +0200

    Add md-selects for team and center

--- app/scripts/login/registerController.js
+++ app/scripts/login/registerController.js
@@ -13,8 +13,28 @@ angular
   .controller('RegisterController', RegisterController);
 
 
-function RegisterController() {
+RegisterController.$inject = ['$resource'];
+function RegisterController($resource) {
   var vm = this;
-  vm.response = {
+
+  vm.user = {};
+  
+  vm.centers = []; 
+  vm.teams = [];
+
+  
+  vm.loadTeams = function() {
+    vm.teams = [];
+    return $resource('http://localhost:1337/team', {populate: '[]', center: vm.user.center.id}).query().$promise.then(function(results) {
+      vm.teams = results;
+    });
   };
+
+  vm.loadCenters = function() {
+    vm.centers = [];
+    return $resource('http://localhost:1337/center', {populate: '[]'}).query().$promise.then(function(results) {
+      vm.centers = results;
+    });
+  };
+
 }
